f24ff77e9cd2b57dcdee0b87715392d9590cb519
Olena Kuchko and Roger Tobias <olena+roger@>
2017-05-09 16:08:39 -0700

    adds updated html, css, and js, which creates the funcionality to select different accounts to deposit or withdrawal into each, while keeping the other accounts the same.

--- js/scripts.js
+++ js/scripts.js
@@ -1,41 +1,70 @@
 //-------------------------------FRONTEND
-var bankAccount;
 $(document).ready(function() {
+  var client = new Client();
 
-  $("#btn-createAccount").click(function(event) {
+  $("#client").submit(function(event) {
     event.preventDefault();
-    // debugger;
+    client.fullName = $("#clientName").val();
+    $("#invitation").prepend("<h1> Welcome, " +client.fullName + "!</h1>")
+    $("#intro").hide();
+    $(".main").show();
+  });
+
+  $("#newAccount").submit(function(event) {
+    event.preventDefault();
+    debugger;
     var inputtedName = $("#name").val();
     var inputtedDeposit = parseInt($("#initialFunds").val());
     var accountType = $("input:radio[name=accountType]:checked").val();
-    bankAccount = new BankAccount(inputtedName, accountType);
-    console.log(bankAccount.addMoney(inputtedDeposit));
+    var bankAccount= new BankAccount(inputtedName, accountType, inputtedDeposit);
+
+    client.addAccount(bankAccount);
+
+    $("ul#accountList").append("<li><span class='displayAccounts'>" + bankAccount.name + " " + bankAccount.type + "</span></li>");
+
+    $(".displayAccounts").last().click(function() {
+      $("#displayBalance").text(bankAccount.balance);
+    });
+    $("#chooseAccount").append("<option value='" + (client.clientList.length-1).toString() + "'>" + bankAccount.name +"</option>" )
+    console.log(client.clientList.length-1);
   });
-  $("#btn-math").click(function(event) {
+
+  $("#accountFunds").submit(function(event) {
     event.preventDefault();
     // debugger;
     var addDeposit = parseInt($("#deposit").val());
     var subtractWithdraw = parseInt($("#withdraw").val());
+    var currentAccount = $("#chooseAccount").val();
 
     if (addDeposit > 0) {
-      bankAccount.addMoney(addDeposit);
+      client.clientList[currentAccount].addMoney(addDeposit);
     }
     else if (subtractWithdraw > 0) {
-      bankAccount.subtractMoney(subtractWithdraw);
+      client.clientList[currentAccount].subtractMoney(subtractWithdraw);
     }
     else {
       alert("Please enter an amount")
     }
-    console.log(bankAccount.balance)
+
+    $("#displayBalance").text(client.clientList[currentAccount].balance);
   });
 });
 
 //-------------------------------BACKEND
-function BankAccount(accountName, accountType) {
+function BankAccount(accountName="", accountType="", accountBalance) {
   this.name = accountName;
   this.type = accountType;
-  this.balance = 0;
+  this.balance = accountBalance;
 }
+
+function Client(fullName) {
+  this.fullName = fullName;
+  this.clientList = [];
+}
+
+Client.prototype.addAccount = function (account) {
+  this.clientList.push(account);
+};
 
 BankAccount.prototype.addMoney = function(amount) {
   return this.balance += amount;
